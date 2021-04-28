ad97c089e5ef15b86709648688912f64a96199f3
Hassan, Omry Emilyo <ohassan@deloitte.it>
2021-04-28 17:11:58 +0200

    first commit

---
+++ src/main/java/com/example/histerhero/HisterheroApplication.java
@@ -0,0 +1,13 @@
+package com.example.histerhero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HisterheroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HisterheroApplication.class, args);
+	}
+
+}
