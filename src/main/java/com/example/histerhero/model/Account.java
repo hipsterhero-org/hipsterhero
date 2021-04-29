fe7e7808eb8bda6a068b2380392797ff139120dd
Hassan, Omry Emilyo <herokutest89@gmail.com>
2021-04-29 17:00:34 +0200

    removed unused property on model

--- src/main/java/com/example/histerhero/model/Account.java
+++ src/main/java/com/example/histerhero/model/Account.java
@@ -3,46 +3,24 @@ package com.example.histerhero.model;
 import javax.persistence.*;
 
 @Entity
-@Table(name="acc")
 public class Account implements SObject{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String accountName;
-    private String billingState;
 
     public Account() {}
 
-    public Account(String firstName, String lastName) {
-        this.accountName = firstName;
-        this.billingState = lastName;
-    }
 
     @Override
     public String toString() {
         return String.format(
                 "Customer[id=%d, firstName='%s', lastName='%s']",
-                id, accountName, billingState);
+                id);
     }
 
     public Long getId() {
         return id;
-    }
-
-    public String getAccountName() {
-        return accountName;
-    }
-
-    public String getBillingState() {
-        return billingState;
-    }
-    public void setAccountName(String accountName) {
-        this.accountName = accountName;
-    }
-
-    public void setBillingState(String billingState) {
-        this.billingState = billingState;
     }
 
 }
